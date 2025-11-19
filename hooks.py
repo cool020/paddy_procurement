102f85cc4ef6e3fa6eca82c99932fbc71e14da98
cool020 <ma.saahim@gmail.com>
2025-11-19 21:39:21 +0530

    Create hooks.py

---
+++ hooks.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from . import __version__ as app_version
+
+app_name = "paddy_procurement"
+app_title = "Paddy Procurement"
+app_publisher = "cool020"
+app_description = "Paddy Procurement App for ERPNext v15"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "you@example.com"
+app_license = "MIT"
+
+# Add app hooks here if you need e.g. doc events, scheduled tasks etc.
+# For now we leave these lists empty so Frappe detects the app.
+doc_events = {}
+scheduler_events = {}
+override_whitelisted_methods = {}
+fixtures = []
